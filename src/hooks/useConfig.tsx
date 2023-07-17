b1879ab2910e636c799258ccd0df398f7cda43f4
Joe from Deving.Zone <git@deving.zone>
2023-07-17 18:59:22 +0200

    fix: remove leaderboardDataEndpoint

--- src/hooks/useConfig.tsx
+++ src/hooks/useConfig.tsx
@@ -3,9 +3,7 @@ import config from "../../public/config.json";
 export const useConfig = () => {
   const chainInfo = config.chain_info;
 
-  const leaderboardDataEndpoint = config.leaderboard_data_endpoint;
-
   const furnaceContractAddress = config.furnace_contract_address;
 
-  return { chainInfo, leaderboardDataEndpoint, furnaceContractAddress };
+  return { chainInfo, furnaceContractAddress };
 };
