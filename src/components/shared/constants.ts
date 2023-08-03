152d10774748e8f7441022bd73ba9ab329767b98
Sen Com <86837735+Sen-Com@users.noreply.github.com>
2023-08-03 12:17:28 +0200

    feat: add address to leaderboard (#34)

--- src/components/shared/constants.ts
+++ src/components/shared/constants.ts
@@ -3,5 +3,6 @@ export const kPageEntries = 5
 export const foundationAddress = 'migaloo1xcu60yphyx5pux5035wqtgf6smvyydcwftz5h5'
 export const KNOWN_ADDRESSES = {
   'migaloo17w97atfwdnjpe6wywwsjjw09050aq9s78jjjsmrmhhqtg7nevpmq0u8t9v':'Eris Protocol',
+  'migaloo1tj69t56h3levwepq4cdmvf8fn59rzhdcqakkc6':'LunaVShape',
   'migaloo1fw0xmf4574hccxsvagc6dzzg37r2gwxtdqdemp': 'EC',
 }
